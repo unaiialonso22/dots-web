73d7d3d048c06495eacad581df18c2993fadf8ee
unaiialonso22 <unaiialonso@gmail.com>
2026-05-11 18:29:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BlogBloqueoCreativo from "./pages/BlogBloqueoCreativo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
